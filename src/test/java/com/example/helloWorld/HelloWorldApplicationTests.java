b4e5c11a392d22ddf401c73815af9243bdf28667
Sagar Melmatti <sagaramelmatti@gmail.com>
2024-03-21 20:56:08 +0530

    Update HelloWorldApplicationTests.java

--- src/test/java/com/example/helloWorld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloWorld/HelloWorldApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.helloWorld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-/*
+
 @SpringBootTest
 class HelloWorldApplicationTests {
 
@@ -11,4 +11,4 @@ class HelloWorldApplicationTests {
 	}
 
 }
-*/
+
